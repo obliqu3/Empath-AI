90c760990dce6c3920f1946bba5723e47fc3991e
Shlok Mirji <mirjishlaok1@gmail.com>
2026-02-14 14:03:10 +0530

    Update auth.py

--- feeler/auth.py
+++ feeler/auth.py
@@ -6,6 +6,6 @@ print(f"Attempting to login with token: {my_token[:5]}...")
 
 try:
     login(token=my_token)
-    print("✅ Success! You are logged in.")
+    print("Success! You are logged in.")
 except Exception as e:
-    print(f"❌ Failed: {e}")
+    print(f"Failed: {e}")
